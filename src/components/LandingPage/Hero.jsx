72397a1f55510012e9fe126df10c4d0a31e95edb
Charles E. Charles <charlescharlesy@gmail.com>
2024-02-20 13:15:02 +0100

    hereo section

--- src/components/LandingPage/Hero.jsx
+++ src/components/LandingPage/Hero.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import backgroundImg from "../../images/book_slider.jpg";
+import book from "../../images/book.png";
 
 const Hero = () => {
-  return (
-    <div>
-      <h1>book</h1>
-    </div>
-  )
-}
+	return (
+		<div
+			className="hero"
+			style={{
+				background: `url(${backgroundImg})`,
+				backgroundPosition: "center",
+			}}
+		>
+			<div className="hero-bg">
+				<div className="hero-info">
+					<h5>WORLD BEST SELLERS</h5>
+					<h2>
+						Escape Book <br /> Charles Fredrick
+					</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nam
+						modi cupiditate soluta veritatis odio culpa minus consectetur aut
+						fugit.
+					</p>
+				</div>
+				<img src={book} alt="Book" height="400px" />
+			</div>
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
